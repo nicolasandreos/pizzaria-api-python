978d460756a53fa98f2dea160ff1b81c91692f9f
Nicolas <nicolasandreose@gmail.com>
2026-06-10 21:25:45 -0300

    chore: add order product schema

--- models.py
+++ models.py
@@ -57,3 +57,14 @@ class Product(Base):
         self.name = name
         self.price = price
         self.description = description
+
+class OrderProduct(Base):
+    __tablename__ = "order_products"
+    order_id = Column("order_id", ForeignKey("orders.id"), nullable=False, primary_key=True)
+    product_id = Column("product_id", ForeignKey("products.id"), nullable=False, primary_key=True)
+    quantity = Column("quantity", Integer, nullable=False)
+    unit_price = Column("unit_price", Float, nullable=False)
+    
+    @property
+    def total_price(self) -> float:
+        return self.quantity * self.unit_price
