ad31684e5bc4a6d1d76496e7791bb27f9e02ed8e
Nicolas <nicolasandreose@gmail.com>
2026-06-11 10:35:02 -0300

    feat: add user register route

--- routes/auth_router.py
+++ routes/auth_router.py
@@ -1,3 +1,22 @@
 from fastapi import APIRouter
+from models import User, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+@auth_router.post("/register")
+async def create_account(name: str, email: str, password: str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+
+    user_by_email = session.query(User)\
+        .filter(User.email == email).first()
+
+    if user_by_email:
+        return {"message": "User with this email already exists"}
+    
+    else:
+        new_user = User(name=name, email=email, password=password)
+        session.add(new_user)
+        session.commit()
+        return {"message": "User created successfully"}
