2decb0e8526e62a45da35d1daa5b233ec13fad93
Nicolas <nicolasandreose@gmail.com>
2026-06-11 15:09:51 -0300

    feat: add user_id to create order

--- schemas/order_schema.py
+++ schemas/order_schema.py
@@ -8,6 +8,7 @@ class OrderItemCreateSchema(BaseModel):
         from_attributes = True
 class RequestCreateOrderSchema(BaseModel):
     items: list[OrderItemCreateSchema]
+    user_id: int
 
     class Config:
         from_attributes = True
