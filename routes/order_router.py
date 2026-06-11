46e4bd8d3c46782ba795be835c82ca2aba649a76
Nicolas <nicolasandreose@gmail.com>
2026-06-11 14:57:41 -0300

    feat: logic to create an order

--- routes/order_router.py
+++ routes/order_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_session
-from models import Order, User
+from models import Order, Product, User, OrderProduct
 from schemas.order_schema import RequestCreateOrderSchema
 
 order_router = APIRouter(prefix="/order", tags=["order"])
@@ -14,9 +14,43 @@ async def create_order(order_schema: RequestCreateOrderSchema, session: Session 
     if not user_by_id:
         raise HTTPException(status_code=404, detail="User not found")
 
-    new_order = Order(user_id=order_schema.user_id, price=order_schema.price)
+    if user_by_id.active == False:
+        raise HTTPException(status_code=403, detail="User is not active and is forbidden to create an order")
+    
+    total_price = 0
+    for product in order_schema.items:
+        product_db = session.query(Product).filter(Product.id == product.product_id).first()
+        if not product_db:
+            raise HTTPException(status_code=404, detail="Product not found")
+        total_price += product_db.price * product.quantity
+
+    new_order = Order(user_id=order_schema.user_id, price=total_price)
 
     session.add(new_order)
+    session.flush()
+
+    for product in order_schema.items:
+        product_db = session.query(Product).filter(Product.id == product.product_id).first()
+
+        if not product_db:
+            raise HTTPException(status_code=404, detail="Product not found")
+
+        order_product = OrderProduct(order_id=new_order.id, product_id=product_db.id, quantity=product.quantity, unit_price=product_db.price)
+        session.add(order_product)
+
     session.commit()
 
-    return HTTPException(status_code=201, detail=f"Order created successfully from User ID: {new_order.user_id} Order ID: {new_order.id} Price: {new_order.price}")
+    return {
+        "message": "Order created successfully",
+        "order_id": new_order.id,
+        "user_id": new_order.user_id,
+        "order_price": new_order.price,
+        "items": [
+            {
+                "product_id": product.product_id,
+                "quantity": product.quantity,
+                "unit_price": product_db.price
+            }
+            for product in order_schema.items
+        ]
+    }
--- schemas/order_schema.py
+++ schemas/order_schema.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 
+
+class OrderItemCreateSchema(BaseModel):
+    product_id: int
+    quantity: int
+    class Config:
+        from_attributes = True
 class RequestCreateOrderSchema(BaseModel):
-    user_id: int
-    price: float
+    items: list[OrderItemCreateSchema]
 
     class Config:
         from_attributes = True
