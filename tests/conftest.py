d43caec8af322cb5fd5da346057121ecc4a0b073
Nicolas <nicolasandreose@gmail.com>
2026-06-17 22:49:42 -0300

    chore: add test database setup

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+import os
+
+from fastapi.testclient import TestClient
+from main import app
+from tests.seed_database import seed_database
+from tests.db_session import TestingSessionLocal, engine
+from database.base import Base
+from dependencies.session_dependencies import get_session
+from models import *
+from services.password_service import PasswordService
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    engine.dispose()
+
+    if os.path.exists("test.db"):
+        os.remove("test.db")
+
+
+@pytest.fixture(autouse=True)
+def clean_tables():
+    db = TestingSessionLocal()
+    seed_database(db)
+
+    yield
+
+    db.query(OrderProduct).delete()
+    db.query(Order).delete()
+    db.query(Product).delete()
+    db.query(User).delete()
+
+    db.commit()
+    db.close()
+
+
+def override_get_session():
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_session] = override_get_session
+
+client = TestClient(app)
---
+++ tests/seed_database.py
@@ -0,0 +1,17 @@
+from models import User
+from services.password_service import PasswordService
+
+def seed_database(db):
+    create_test_users(db)
+    
+
+def create_test_users(db):
+    user = User(name="test", email="test@example.com", password=PasswordService.hash_password("123456"))
+    admin_user = User(name="admin", email="admin@example.com", password=PasswordService.hash_password("123456"), admin=True)
+    inactive_user = User(name="inactive", email="inactive@example.com", password=PasswordService.hash_password("123456"), active=False)
+    db.add(user)
+    db.add(admin_user)
+    db.add(inactive_user)
+    db.commit()
+
+    
