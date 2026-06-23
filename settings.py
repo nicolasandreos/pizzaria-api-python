3a369988b6a576c0afa6b3f9e6c2616cc49e5464
Nicolas <nicolasandreose@gmail.com>
2026-06-23 17:45:11 -0300

    feat: ensure pattern values are set in settings file

--- settings.py
+++ settings.py
@@ -21,9 +21,9 @@ else:
 
 class Settings:
     ENVIRONMENT = environment
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    JWT_TOKEN = os.getenv("JWT_TOKEN")
-    ALGORITHM = os.getenv("ALGORITHM")
+    DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456@mysql:3306/pizzaria")
+    JWT_TOKEN = os.getenv("JWT_TOKEN", "secret")
+    ALGORITHM = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
     REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", "10080"))
     RUN_DATABASE_MIGRATIONS = os.getenv("RUN_DATABASE_MIGRATIONS", "False").lower() == "true"
